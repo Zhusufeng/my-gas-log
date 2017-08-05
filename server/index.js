d54ab529d756e6df9544b25070597485a9d2effe
Lisa Gee <ZhuSuFeng@gmail.com>
2017-08-04 19:32:12 -0700

    Update Express to serve static files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+// Serve static file
+app.use(express.static('compiled/client'));
+
+// Basic GET to /
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello you did a GET request');
 });
 
+// Basic POST to /
+app.post('/', function(request, response) {
+  response.send('Got a POST request');
+})
+
+// Ensure server is listening
 app.listen(8080, function() {
   console.log('My Gas Log server listening on port 8080');
 })
