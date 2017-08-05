92ee63756aa1786e03607a2515d25a792b32b5ad
Lisa Gee <ZhuSuFeng@gmail.com>
2017-08-05 15:07:46 -0700

    Connect to mySQL and create database mygaslog

--- server/index.js
+++ server/index.js
@@ -1,6 +1,38 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mysql= require('mysql');
+
+var db = mysql.createConnection({
+  user: 'root',
+  password: 'root',
+});
+
+db.connect(function(err) {
+  if (err) {
+    console.log('You have an error connecting to the db: ', err);
+  }
+  console.log('!!! Connected to MySQL !!!');
+
+  if (!db.database) {
+    console.log('Database mygaslog does not exist');
+
+    var createDB = "CREATE DATABASE mygaslog";
+
+    db.query(createDB, function(err, result) {
+      if (err) {
+        console.log('Did not create database mygaslog. Error is ', err);
+      }
+      console.log('Database mygaslog created');
+    });
+
+  } else {
+    console.log('Database named mygaslog is connected');
+  }
+
+
+
+});
 
 // Parse application/json files
 app.use(bodyParser.json());
