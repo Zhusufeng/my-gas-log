c3534d4815def07722e3d963263740cd7553a580
Lisa Gee <ZhuSuFeng@gmail.com>
2017-08-05 11:14:36 -0700

    Get body-parser for Express to work on server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,35 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// Parse application/json files
+app.use(bodyParser.json());
+
+// Parse text files
+// app.use(bodyParser.text());
 
 // Serve static file
 app.use(express.static('compiled/client'));
 
 // Basic GET to /
-app.get('/', function(request, response) {
-  response.send('Hello you did a GET request');
+app.get('/', function(req, res) {
+  res.send('Hello you did a GET request');
 });
 
 // Basic POST to /
-app.post('/', function(request, response) {
-  response.send('Got a POST request');
-})
+app.post('/', function(req, res) {
+  res.status(201).send(req.body);
+});
+
+// Basic POST to /post
+app.post('/post', function(req, res) {
+  // Express body-parser
+  console.log('POSTED. Server side says success: ', req.body);
+  res.status(201).send(req.body);
+
+});
 
 // Ensure server is listening
 app.listen(8080, function() {
   console.log('My Gas Log server listening on port 8080');
-})
+});
